e1dfe288fbdd8c99c5694718334496410d4f4a75
Guadman98 <guadmanmen0722@gmail.com>
2024-04-25 19:06:15 -0500

    Actualziación conceptos de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-import { CounterModule } from './counter/components/counter/counter.module';
-
 import { AppComponent } from './app.component';
+
+import { CounterModule } from './counter/components/counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule
   ],
   providers: [],
